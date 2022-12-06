3acfdab174590a3442f2e9b99c2cb4c8459383f4
jhsseonn <hosu0125@gmail.com>
2022-12-06 17:33:53 +0900

    style: (StringParams) GRAPH_STANDARA -> GRAPH_STANDARD 오타 수정

--- src/main/java/subway/config/StringParams.java
+++ src/main/java/subway/config/StringParams.java
@@ -5,7 +5,7 @@ public class StringParams {
             "1. 경로 조회\n" +
             "Q. 종료\n";
     public final static String INPUT_CHOICE = "## 원하는 기능을 선택하세요.\n";
-    public final static String GRAPH_STANDARA = "## 경로 기준\n" +
+    public final static String GRAPH_STANDARD = "## 경로 기준\n" +
             "1. 최단 거리\n" +
             "2. 최소 시간\n" +
             "B. 돌아가기\n";
