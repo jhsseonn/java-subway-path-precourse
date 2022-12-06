ac0e987e43734c88f0e3d901602aaa4acd55c7e6
jhsseonn <hosu0125@gmail.com>
2022-12-06 16:37:03 +0900

    feat: (StationController) StationRepository에 역 정보 저장 기능 추가

--- src/main/java/subway/controller/StationController.java
+++ src/main/java/subway/controller/StationController.java
@@ -1,4 +1,23 @@
 package subway.controller;
 
+import subway.domain.Station;
+import subway.repository.StationRepository;
+
 public class StationController {
+    public void saveStationList() {
+        Station gyodae = new Station("교대역");
+        Station gangnam = new Station("강남역");
+        Station yeoksam = new Station("역삼역");
+        Station nambu = new Station("남부터미널역");
+        Station yangjae = new Station("양재역");
+        Station yangjaeCitizen = new Station("양재시민의숲역");
+        Station maebong = new Station("매봉역");
+        StationRepository.addStation(gyodae);
+        StationRepository.addStation(gangnam);
+        StationRepository.addStation(yeoksam);
+        StationRepository.addStation(nambu);
+        StationRepository.addStation(yangjae);
+        StationRepository.addStation(yangjaeCitizen);
+        StationRepository.addStation(maebong);
+    }
 }
