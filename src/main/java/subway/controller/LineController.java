c62b7520754e09bffa8a827e55481591b528e484
jhsseonn <hosu0125@gmail.com>
2022-12-06 16:32:21 +0900

    style: (LineController) 파일명 오타 수정, setLineList -> saveLineList로 함수명 수정

--- src/main/java/subway/controller/LineController.java
+++ src/main/java/subway/controller/LineController.java
@@ -3,8 +3,8 @@ package subway.controller;
 import subway.domain.Line;
 import subway.repository.LineRepository;
 
-public class LineConroller {
-    public void setLineList() {
+public class LineController {
+    public void saveLineList() {
         Line secondLine = new Line("2호선");
         Line thirdLine = new Line("3호선");
         Line shinbundangLine = new Line("신분당선");
