ed361b6f53e3dc359ace76aca6df8ccfd124a1c6
jhsseonn <hosu0125@gmail.com>
2022-12-06 16:30:37 +0900

    feat: (LineController) 노선 종류 repository에 저장 기능 추가

--- src/main/java/subway/controller/LineConroller.java
+++ src/main/java/subway/controller/LineConroller.java
@@ -4,5 +4,12 @@ import subway.domain.Line;
 import subway.repository.LineRepository;
 
 public class LineConroller {
-
+    public void setLineList() {
+        Line secondLine = new Line("2호선");
+        Line thirdLine = new Line("3호선");
+        Line shinbundangLine = new Line("신분당선");
+        LineRepository.addLine(secondLine);
+        LineRepository.addLine(thirdLine);
+        LineRepository.addLine(shinbundangLine);
+    }
 }
