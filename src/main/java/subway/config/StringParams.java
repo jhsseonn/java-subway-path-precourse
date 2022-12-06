166a906ee25e1828cd0f8fce98d8a50e23be6486
jhsseonn <hosu0125@gmail.com>
2022-12-06 17:23:28 +0900

    feat: (StringParams) 출력 문구 static String 상수 추가

---
+++ src/main/java/subway/config/StringParams.java
@@ -0,0 +1,17 @@
+package subway.config;
+
+public class StringParams {
+    public final static String MAIN = "## 메인 화면\n" +
+            "1. 경로 조회\n" +
+            "Q. 종료\n";
+    public final static String INPUT_CHOICE = "## 원하는 기능을 선택하세요.\n";
+    public final static String GRAPH_STANDARA = "## 경로 기준\n" +
+            "1. 최단 거리\n" +
+            "2. 최소 시간\n" +
+            "B. 돌아가기\n";
+    public final static String INPUT_START="## 출발역을 입력하세요.\n";
+    public final static String INPUT_DESTINATION = "## 도착역을 입력하세요.\n";
+    public final static String READ_RESULT = "## 조회 결과\n";
+    public final static String INFO_OUTPUT = "[INFO]";
+    public final static String ERROR_OUTPUT = "[ERROR]";
+}
