892b1c931ca9ea0d6e2afdf1acda833d05ae569a
jhsseonn <hosu0125@gmail.com>
2022-12-06 17:36:26 +0900

    feat: (OutputView) output 구문 출력 기능 추가

---
+++ src/main/java/subway/config/OutputView.java
@@ -0,0 +1,30 @@
+package subway.config;
+
+import java.awt.*;
+
+public class OutputView extends StringParams{
+    public void printMain() {
+        System.out.print(MAIN);
+    }
+
+    public void printInpucChoice() {
+        System.out.print(INPUT_CHOICE);
+    }
+
+    public void printGraphStandard() {
+        System.out.print(GRAPH_STANDARD);
+    }
+
+    public void printInputStart() {
+        System.out.print(INPUT_START);
+    }
+
+    public void printInputDestination() {
+        System.out.print(INPUT_DESTINATION);
+    }
+
+    public void printReadResult() {
+        System.out.print(READ_RESULT);
+    }
+}
+
